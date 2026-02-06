d6612f9e10ac642cb7253ec53c8d7243d6cae896
CodeWithPawan15 <pavanborde.personal97@gmail.com>
2026-02-07 01:38:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,24 @@ const heartLoader = document.querySelector(".cssload-main");
 const yesBtn = document.querySelector(".js-yes-btn");
 const noBtn = document.querySelector(".js-no-btn");
 
-// move NO button
+// /change the postion of no button
 noBtn.addEventListener("mouseover", () => {
   const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
   const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
 
-  noBtn.style.left = ${newX}px;
-  noBtn.style.top = ${newY}px;
+  noBtn.style.left = `${newX}px`;
+  noBtn.style.top = `${newY}px`;
 });
 
-// YES button
+// yes button functionality
+
 yesBtn.addEventListener("click", () => {
   questionContainer.style.display = "none";
   heartLoader.style.display = "inherit";
 
-  setTimeout(() => {
+  const timeoutId = setTimeout(() => {
     heartLoader.style.display = "none";
-    resultContainer.style.display = "block";
-    // ❌ gifResult.play();  <-- removed
+    resultContainer.style.display = "inherit";
+    gifResult.play();
   }, 3000);
 });
